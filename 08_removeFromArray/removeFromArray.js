3a4d1f83c7c25874cb7cf39234fbd48b165967df
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-05 16:11:48 +0530

    Completed exercise 09

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray,...elements) {
+        // code for multiple elemets to be removed
+        for(let i = 0; i < elements.length; i++) {
+            const locateElement = (item) => item === elements[i];
+            let foundElement = inputArray.filter(locateElement);
+            console.log(foundElement);
+            for (let j = 0; j < foundElement.length; j++) {
+                let whereAt = inputArray.indexOf(foundElement[j]);
+                console.log(whereAt);
+                if (whereAt !== -1) {
+                    inputArray.splice(whereAt,1);
+                }
+                    
+            }
+            
+        }    
+        return inputArray;
+
 };
 
 // Do not edit below this line
