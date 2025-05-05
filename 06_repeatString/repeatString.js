39b21d081b1d0b39ae1db1e6ff408819c1e41f37
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-05 11:41:14 +0530

    Completed exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(original,repeatTimes) {
+    let newString = '';
+    if (repeatTimes < 0) {
+        return "ERROR";
 
+    }
+    for(let i=0; i < repeatTimes; i++) {
+        newString = newString + original;
+    }
+    return newString;
 };
 
 // Do not edit below this line
