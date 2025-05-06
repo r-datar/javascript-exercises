390d8127ff8fa5103736496a3012e3c93d557f2d
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-06 15:28:08 +0530

    Completed exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    //checks for negative number
+    if (startNum < 0 || endNum < 0 ) {
+        return ("ERROR");
+    }  
+    // checks for decimal numbers 
+    regex = /[.]/;
+    if (regex.test(startNum) || regex.test(endNum)) {
+        return ("ERROR");
+    }
 
+    if(typeof startNum !== "number" || typeof endNum !== "number")  {
+        return("ERROR");
+    }
+
+    // code starts
+    let sum = 0, start = 0, end = 0;
+    if ( startNum > endNum ) {
+        start = endNum ; 
+        end = startNum ;   
+    } 
+    else if ( startNum < endNum) {
+        start = startNum;
+        end = endNum;
+    }
+
+    for(let i=start ; i<=end ; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
