7ce2fe78745856d3e222f4803523f93b59ac618c
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-06 16:03:39 +0530

    Completed exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32 ) * ( 5 / 9) ;
+  return cel = Number.isInteger(cel) ? +cel : +cel.toFixed(1) 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // f = (c * 9 / 5 ) + 32 
+  let fahr = (temp * ( 9 / 5) ) + 32 ;
+  return fahr = Number.isInteger(fahr) ? +fahr : +fahr.toFixed(1) 
 };
 
 // Do not edit below this line
