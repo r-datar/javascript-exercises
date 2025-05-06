35c3baf6d040b7045549cdef6cfed5deb479329e
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-06 15:50:39 +0530

    Completed exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(checkYear) {
+    let retVal = false;
+    if (checkYear % 100 == 0 ) {
+        if (checkYear % 400 == 0) {
+            retVal = true;
+        }
+    }
+    else if (checkYear % 4 == 0) {
+        retVal = true;
+    }
+    
+    return retVal;       
 };
 
 // Do not edit below this line
