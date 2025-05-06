a789eb042c388e8a5b21204a7d35402a6699ddc2
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-06 16:36:57 +0530

    Completed exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 -  num2 ;
 	
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let answer = 0;
+	if(Array.isArray(numbers)) {
+    for (let i = 0; i < numbers.length; i++) {
+      answer += numbers[i];
+    }
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let answer = 1;
+	if(Array.isArray(numbers)) {
+    for (let i = 0; i < numbers.length; i++) {
+      answer *= numbers[i];
+    }
+  }
+  return answer;
 };
 
-const power = function() {
+const power = function(base,power) {
 	
+  return Math.pow(base,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let facto = 1;
+  if (num > 0) {
+    for(let i = num; i > 0 ; i--) {
+      facto *= i;
+    }
+  }
+  
+  return facto;
 };
 
 // Do not edit below this line
