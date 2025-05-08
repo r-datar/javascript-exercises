c3f41edc29c6208836cac16dfa5379de5ea64d20
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-08 16:06:07 +0530

    Completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    
+    if (typeof nth === "string") nth = +nth;
+    if (nth < 0) return "OOPS";
+    if (nth == 0) return 0;
+    if (nth == 1 || nth == 2) return 1;
 
+
+    let start = [0,1];
+    let addition = 0;
+
+    for(let i = 2; i <= nth; i++) {
+    
+        addition = start[i-1] + start[i - 2];
+        start[i] = addition;
+       
+    }
+    return addition;
 };
 
 // Do not edit below this line
