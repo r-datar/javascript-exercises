5c82a54cb5ab5b69b3f819807c07785d71f831e3
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-08 10:36:07 +0530

    Completed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (palinString) {
+    // remove punctuation
+    palinString = palinString.replace(/\W/g, '')
+    //make all lowercase
+    palinString = palinString.toLowerCase();
+    // get midpoint and end
+    let len = palinString.length;
+    let midPoint = 0, secondHalf; 
+    if (len % 2 ) {
+        midPoint = Math.floor(len  / 2) + 1;
+        secondHalf = palinString.slice(midPoint - 1)
+    }  else {
+      midPoint =   len / 2;
+      secondHalf = palinString.slice(midPoint);
+    } 
+    let firstHalf =  palinString.slice(0,midPoint);
+    secondHalf = secondHalf.split('').reverse().join('');
 
+    return firstHalf === secondHalf;
+    
+    
 };
 
 // Do not edit below this line
