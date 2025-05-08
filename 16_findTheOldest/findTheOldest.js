3cf3f76f340bddaeb226b3b04c898564bf4ea738
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-08 17:05:29 +0530

    Completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findAge = function (birthYear, deathYear) {
+   if(!deathYear) {
+    deathYear = new Date().getFullYear();
+   }
+   return deathYear - birthYear;
+}
 
+
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldestPerson, thisPerson) => {
+            let oldestPersonAge = findAge(oldestPerson.yearOfBirth,oldestPerson.yearOfDeath);
+            let thisPersonAge = findAge(thisPerson.yearOfBirth,thisPerson.yearOfDeath);
+
+            return oldestPersonAge > thisPersonAge ? oldestPerson : thisPerson ;
+        });
 };
 
 // Do not edit below this line
